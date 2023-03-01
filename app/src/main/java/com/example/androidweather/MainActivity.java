8cb50eb62d672f5ccac824b361c8399ed9be0d7f
wierdwizerd <115394561+wierdwizerd@users.noreply.github.com>
2023-03-01 05:57:57 -0800

    Working Prototype

--- app/src/main/java/com/example/androidweather/MainActivity.java
+++ app/src/main/java/com/example/androidweather/MainActivity.java
@@ -2,23 +2,104 @@ package com.example.androidweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
-EditText etCity, etCountry;
-TextView tvResults;
-private final String url = "http://api.openweathermap.org/data/2.5/weather";
-private final String appid = "a2eb7ee73020d31b3a6c48f6b51b44f6";
+    EditText etCity, etCountry;
+    TextView tvResults;
+    private final String url = "http://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "a2eb7ee73020d31b3a6c48f6b51b44f6";
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etCity = findViewById(R.id.etCity);
+        etCountry = findViewById(R.id.etCountry);
+        tvResults = findViewById(R.id.tvResults);
+
     }
 
     public void getWeatherDetails(View view) {
+        String tempUrl = "";
+        String city = etCity.getText().toString().trim();
+        String country = etCountry.getText().toString().trim();
+        String unit = "imperial";
+
+        if (city.equals("")) {
+            tvResults.setText("City field can not be empty");
+        } else {
+            if (!country.equals("")) {
+                tempUrl = url + "?q=" + city + "," + country + "&units=" + unit + "&appid=" + appid;
+            } else {
+                tempUrl = url + "?q=" + city + "&units=" + unit + "&appid=" + appid;
+            }
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>(){
+                @Override
+                public void onResponse(String response) {
+                    //Log.d("response", response);
+                    String output = "";
+                    try {
+                        JSONObject jsonResponse = new JSONObject(response);
+                        JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                        JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String description = jsonObjectWeather.getString("description");
+                        JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                        double temp = jsonObjectMain.getDouble("temp");
+                        double feelsLike = jsonObjectMain.getDouble("feels_like");
+                        float pressure = jsonObjectMain.getInt("pressure");
+                        int humidity = jsonObjectMain.getInt("humidity");
+                        JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                        String wind = jsonObjectWind.getString("speed");
+                        JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                        String clouds = jsonObjectClouds.getString("all");
+                        JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                        String countryName = jsonObjectSys.getString("country");
+                        String cityName = jsonResponse.getString("name");
+                        tvResults.setTextColor(Color.rgb(255,255,255));
+                        output += "Current weather of " + cityName + " (" + countryName + ")"
+                                + "\n Temp: " + df.format(temp) + " °F"
+                                + "\n Feels Like: " + df.format(feelsLike) + " °F"
+                                + "\n Humidity: " + humidity + "%"
+                                + "\n Description: " + description
+                                + "\n Wind Speed: " + wind + "mph"
+                                + "\n Cloudiness: " + clouds + "%"
+                                + "\n Pressure: " + pressure + " hPa";
+                        tvResults.setText(output);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(),
+                            error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
     }
 }
