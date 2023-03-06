e916b48bda2f1e66c1ea2fb32769558f238ba0bc
wierdwizerd <115394561+wierdwizerd@users.noreply.github.com>
2023-03-06 14:14:23 -0800

    Final Template

--- app/src/main/java/com/example/androidweather/MainActivity.java
+++ app/src/main/java/com/example/androidweather/MainActivity.java
@@ -26,7 +26,7 @@ import java.text.DecimalFormat;
 public class MainActivity extends AppCompatActivity {
     EditText etCity, etCountry;
     TextView tvResults;
-    private final String url = "http://api.openweathermap.org/data/2.5/weather";
+    private final String url = "http://api.openweathermap.org/data/2.5/";
     private final String appid = "a2eb7ee73020d31b3a6c48f6b51b44f6";
     DecimalFormat df = new DecimalFormat("#.##");
 
@@ -50,19 +50,20 @@ public class MainActivity extends AppCompatActivity {
             tvResults.setText("City field can not be empty");
         } else {
             if (!country.equals("")) {
-                tempUrl = url + "?q=" + city + "," + country + "&units=" + unit + "&appid=" + appid;
+                tempUrl = url + "weather" + "?q=" + city + "," + country + "&units=" + unit + "&appid=" + appid;
             } else {
-                tempUrl = url + "?q=" + city + "&units=" + unit + "&appid=" + appid;
+                tempUrl = url + "weather" + "?q=" + city + "&units=" + unit + "&appid=" + appid;
             }
             StringRequest stringRequest = new StringRequest(Request.Method.POST, tempUrl, new Response.Listener<String>(){
                 @Override
                 public void onResponse(String response) {
-                    //Log.d("response", response);
+                    Log.d("response", response);
                     String output = "";
                     try {
                         JSONObject jsonResponse = new JSONObject(response);
                         JSONArray jsonArray = jsonResponse.getJSONArray("weather");
                         JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                        String main = jsonObjectWeather.getString("main");
                         String description = jsonObjectWeather.getString("description");
                         JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
                         double temp = jsonObjectMain.getDouble("temp");
@@ -78,10 +79,12 @@ public class MainActivity extends AppCompatActivity {
                         String cityName = jsonResponse.getString("name");
                         tvResults.setTextColor(Color.rgb(255,255,255));
                         output += "Current weather of " + cityName + " (" + countryName + ")"
+                                + "\n" + main
                                 + "\n Temp: " + df.format(temp) + " °F"
                                 + "\n Feels Like: " + df.format(feelsLike) + " °F"
                                 + "\n Humidity: " + humidity + "%"
                                 + "\n Description: " + description
+//                                + "\n Rainfall: " + rain + "inches"
                                 + "\n Wind Speed: " + wind + "mph"
                                 + "\n Cloud Cover: " + clouds + "%"
                                 + "\n Barometric Pressure: " + df.format(pressure * 0.02953) + " inches";
