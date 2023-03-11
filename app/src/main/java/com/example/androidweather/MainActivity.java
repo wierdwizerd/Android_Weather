a9c4cdc69aa9bf7b6af6c09dfede77e93cc0565f
wierdwizerd <115394561+wierdwizerd@users.noreply.github.com>
2023-03-11 14:29:30 -0800

    Added buttons anc changed tempUrl to access 24 hour and 5 day forecasts (Currently all button get the current weather)

--- app/src/main/java/com/example/androidweather/MainActivity.java
+++ app/src/main/java/com/example/androidweather/MainActivity.java
@@ -18,7 +18,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    EditText etCity, etCountry;
+    EditText etCity;
     TextView tvResults;
 
     @Override
@@ -26,16 +26,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         etCity = findViewById(R.id.etCity);
-        etCountry = findViewById(R.id.etCountry);
         tvResults = findViewById(R.id.tvResults);
 
     }
 
     @SuppressLint("SetTextI18n")
     public void getWeatherDetails(View view) {
-        String tempUrl;
+        String tempUrl = "";
         String city = etCity.getText().toString().trim();
-        String country = etCountry.getText().toString().trim();
 
 
         if (city.equals("")) {
@@ -43,10 +41,8 @@ public class MainActivity extends AppCompatActivity {
         } else {
             String appid = "be082770d45d4fc781332903230703";
             String url = "http://api.weatherapi.com/v1/";
-            if (!country.equals("")) {
-                tempUrl = url + "current.json" + "?q=" + city + "," + country + "&key=" + appid;
-            } else {
-                tempUrl = url + "current.json"  + "?key=" + appid + "&q=" + city;
+
+                tempUrl = url + "forecast.json"  + "?key=" + appid + "&q=" + city;
             }
             StringRequest stringRequest = new StringRequest(Request.Method.GET, tempUrl, response -> {
                Log.d("response", response);
@@ -55,31 +51,30 @@ public class MainActivity extends AppCompatActivity {
                    JSONObject jsonResponse = new JSONObject(response);
                     JSONObject jsonObjectCurrent = jsonResponse.getJSONObject("current");
                     JSONObject jsonObjectCondition = jsonObjectCurrent.getJSONObject("condition");
-                    String textObj = jsonObjectCondition.getString("text");
+                    String currentCondition = jsonObjectCondition.getString("text");
 //                    String iconObj = jsonObjectCondition.getString("icon");
                     JSONObject jsonObjectLocation = jsonResponse.getJSONObject("location");
                     double temp_f = jsonObjectCurrent.getDouble("temp_f");
                     double feelslike_f = jsonObjectCurrent.getDouble("feelslike_f");
-                    float pressure_in = jsonObjectCurrent.getInt("pressure_in");
-                    int humidity = jsonObjectCurrent.getInt("humidity");
+//                    float pressure_in = jsonObjectCurrent.getInt("pressure_in");
+//                    int humidity = jsonObjectCurrent.getInt("humidity");
                     double wind_mph = jsonObjectCurrent.getDouble("wind_mph");
                     double gust_mph = jsonObjectCurrent.getDouble("gust_mph");
                     int clouds = jsonObjectCurrent.getInt("cloud");
                     float  precip_in = jsonObjectCurrent.getInt("precip_in");
-                       String stateName = jsonObjectLocation.getString("region");
+                       String regionName = jsonObjectLocation.getString("region");
                     String cityName = jsonObjectLocation.getString("name");
                     tvResults.setTextColor(Color.rgb(255,255,255));
-                    output += "Current weather of " + cityName
-                            + ", " + stateName
-                            + "\n" + textObj
+                    output += cityName + ", " + regionName + "\n"
+                            + "\n Current weather: " + currentCondition
                             + "\n Temp: " + temp_f + " °F"
                             + "\n Feels Like: " + feelslike_f + " °F"
                             + "\n Wind Speed: " + wind_mph + "mph"
                             + "\n Wind Gust: " + gust_mph + "mph"
                             + "\n Cloud Cover: " + clouds + "%"
-                            + "\n Rainfall: " + precip_in + " in."
-                            + "\n Humidity: " + humidity + "%"
-                            + "\n Barometric Pressure: " + pressure_in + " in.";
+                            + "\n Rainfall: " + precip_in + " in.";
+//                            + "\n Humidity: " + humidity + "%"
+//                            + "\n Barometric Pressure: " + pressure_in + " in.";
                     tvResults.setText(output);
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -90,4 +85,3 @@ public class MainActivity extends AppCompatActivity {
             MySingleton.getInstance(MainActivity.this).addToRequestQueue(stringRequest);
         }
    }
-}
